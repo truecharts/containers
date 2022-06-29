9b00e4dae4d63846be1ef24166b9a3d382755456
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-06-29 22:37:43 +0200

    Update jumpstart.sh

--- mirror/nextcloud-fpm/includes/jumpstart.sh
+++ mirror/nextcloud-fpm/includes/jumpstart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -n "${NEXTCLOUD_TRUSTED_DOMAINS+x}" ]; then
-    echo "Setting trusted domains…"
+    echo "ReSetting trusted domains…"
     NC_TRUSTED_DOMAIN_IDX=1
     for DOMAIN in $NEXTCLOUD_TRUSTED_DOMAINS ; do
         DOMAIN=$(echo "$DOMAIN" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
@@ -10,5 +10,18 @@ if [ -n "${NEXTCLOUD_TRUSTED_DOMAINS+x}" ]; then
     done
 fi
 run_as "php /var/www/html/occ app:install notify_push"
+
+echo "Waiting for HPB-Container to start..."
+until [ -f /var/www/html/REQHPB ]
+do
+     sleep 10
+     echo "HPB-Container not found... waiting..."
+done
+echo  "HPB-Container found!"
+echo  "Waiting for 20 seconds before High Performance Backend setup..."
+sleep 20
+echo  "Configuring High Performance Backend for url: ${ACCESS_URL}"
+run_as "php /var/www/html/occ notify_push:setup ${ACCESS_URL}/push
+rm -f /var/www/html/REQHPB
 
 exec "$@"
