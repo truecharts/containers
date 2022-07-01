44a5885effabcd26182e25f15e820486ba41ce34
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-07-01 13:12:31 +0200

    remove HPB initialisation from entrypoint.

--- mirror/nextcloud-fpm/includes/jumpstart.sh
+++ mirror/nextcloud-fpm/includes/jumpstart.sh
@@ -9,19 +9,5 @@ if [ -n "${NEXTCLOUD_TRUSTED_DOMAINS+x}" ]; then
         NC_TRUSTED_DOMAIN_IDX=$(($NC_TRUSTED_DOMAIN_IDX+1))
     done
 fi
-[ -f /var/www/html/custom_apps/notify_push/bin/x86_64/notify_push ] && echo "installing notify_push already seems to be installed..." || run_as "php /var/www/html/occ app:install notify_push"
-
-echo "Waiting for HPB-Container to start..."
-until [ -f /var/www/html/REQHPB ]
-do
-     sleep 10
-     echo "HPB-Container not found... waiting..."
-done
-echo  "HPB-Container found!"
-echo  "Waiting for 20 seconds before High Performance Backend setup..."
-sleep 20
-echo  "Configuring High Performance Backend for url: ${ACCESS_URL}"
-run_as "php /var/www/html/occ notify_push:setup ${ACCESS_URL}/push"
-rm -f /var/www/html/REQHPB
 
 exec "$@"
