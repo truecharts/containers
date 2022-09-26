791a20ac02375db5f2e7b9049cd767fa5e9b38ae
Stavros kois <s.kois@outlook.com>
2022-09-26 15:23:29 +0300

    fix version

--- base/ubuntu/latest-version.sh
+++ base/ubuntu/latest-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-TOKEN=$(curl https://ghcr.io/token\?scope\="repository:onedr0p/ubuntu-jammy:pull" | jq --raw-output '.[]')
-version=$(curl -H "Authorization: Bearer ${TOKEN}" https://ghcr.io/v2/onedr0p/ubuntu-jammy/tags/list | jq --raw-output '.tags[]' | grep -v latest | grep -v buildcache | tail -n1)
+TOKEN=$(curl https://ghcr.io/token\?scope\="repository:onedr0p/ubuntu:pull" | jq --raw-output '.[]')
+version=$(curl -H "Authorization: Bearer ${TOKEN}" https://ghcr.io/v2/onedr0p/ubuntu/tags/list | jq --raw-output '.tags[]' | grep -v latest | grep -v buildcache | tail -n1)
 version="${version#*v}"
 version="${version#*release-}"
 echo "${version}"
