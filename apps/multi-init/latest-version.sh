0c73262f8d774882c9773a129028fe181db40038
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-12-24 18:58:42 +0100

    oops

--- apps/multi-init/latest-version.sh
+++ apps/multi-init/latest-version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "$(curl -s https://pkgs.alpinelinux.org/package/edge/testing/x86_64/kubectl | grep "Flag this package out of date" | sed -e 's/<[^>]*>//g' | sed -e 's/ //g')"
-
+version=1.1.0
+printf "%s" "${version}"
