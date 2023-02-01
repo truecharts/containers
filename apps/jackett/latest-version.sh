5c234c215f2418902a32e9ab95632a8caa93d478
xstar97 <dev.xstar97@gmail.com>
2023-02-01 09:06:22 -0500

    update ubuntu and version script

--- apps/jackett/latest-version.sh
+++ apps/jackett/latest-version.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+version=$(curl -sX GET "https://api.github.com/repos/Jackett/Jackett/releases/latest" | jq --raw-output '.tag_name')
+version="${version#*v}"
+version="${version#*release-}"
 
-printf "%s" "0.20.2814"
+printf "%s" "${version}"
