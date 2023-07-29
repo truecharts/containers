69f0bdb9e26147045e88314d8aaa4bd332107768
Stavros kois <s.kois@outlook.com>
2023-07-29 14:17:57 +0300

    script

---
+++ mirror/traefik/clone-plugins.sh
@@ -0,0 +1,13 @@
+for plugin in $(env | grep "$PLUGIN_PREFIX");
+do
+  # Get the variable name
+  var_name=$(echo "${plugin}" | cut -d '=' -f1);
+  # Get the plugin repo from the variable
+  plugin_repo=$(echo "${plugin}" | cut -d '=' -f2);
+  # Get the version from the VERSION Prefixed variable
+  version=$(echo "$VERSION_PREFIX$var_name" | cut -d '=' -f2);
+  echo "Cloning ${plugin_repo} at ${version} into /plugins-local/src/${plugin_repo}";
+  # Clone the single "branch" (tag) into the plugins-local folder
+  git clone "https://${plugin_repo}" "/plugins-local/src/${plugin_repo}"
+    --depth 1 -branch "${version}" --single-branch;
+done
