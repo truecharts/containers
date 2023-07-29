f1d1319c2fc3aa45e94f06f7471b4bf687ae5e8e
Stavros kois <s.kois@outlook.com>
2023-07-29 14:45:16 +0300

    fix prefx

--- mirror/traefik/clone-plugins.sh
+++ mirror/traefik/clone-plugins.sh
@@ -1,12 +1,13 @@
 for plugin in $(env | grep "$PLUGIN_PREFIX");
 do
   # Get the variable name
-  plugin_name=$(echo "${plugin}" | cut -d '=' -f1 | cut -d "$PLUGIN_PREFIX" -f2);
+  plugin_name=$(echo "${plugin}" | cut -d '=' -f1);
+  plugin_name=${plugin_name#"${PLUGIN_PREFIX}"}
   # Get the plugin repo from the variable
   plugin_repo=$(echo "${plugin}" | cut -d '=' -f2);
   # Get the version from the VERSION Prefixed variable
   version=$(echo "$VERSION_PREFIX$plugin_name" | cut -d '=' -f2);
-  echo "Cloning ${plugin_repo} at ${version} into /plugins-local/src/${plugin_repo}";
+  echo "${plugin_name}: Cloning ${plugin_repo} at ${version} into /plugins-local/src/${plugin_repo}";
   # Clone the single "branch" (tag) into the plugins-local folder
   git clone "https://${plugin_repo}" "/plugins-local/src/${plugin_repo}"
     --depth 1 -branch "${version}" --single-branch;
