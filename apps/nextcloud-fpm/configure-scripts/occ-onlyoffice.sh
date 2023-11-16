670ac3c685d74f534bb1fca4e8426a7b78075755
Stanislav Dimov <stanislav.vdimov@gmail.com>
2023-11-16 01:06:25 +0000

    Add onlyoffice DocumentServerInternalUrl StorageUrl  and verify_peer_off configs

--- apps/nextcloud-fpm/configure-scripts/occ-onlyoffice.sh
+++ apps/nextcloud-fpm/configure-scripts/occ-onlyoffice.sh
@@ -4,6 +4,13 @@ occ_onlyoffice_install() {
   install_app onlyoffice
 
   occ config:app:set onlyoffice DocumentServerUrl --value="${NX_ONLYOFFICE_URL:?"NX_ONLYOFFICE_URL is unset"}"
+  occ config:app:set onlyoffice DocumentServerInternalUrl --value="${NX_ONLYOFFICE_INTERNAL_URL:-""}"
+  occ config:app:set onlyoffice StorageUrl --value="${NX_ONLYOFFICE_NEXTCLOUD_INTERNAL_URL:-""}"
+  if [ "${NX_ONLYOFFICE_VERIFY_SSL:-"true"}" = "false" ]; then
+    occ config:app:set onlyoffice verify_peer_off --value="false"
+  else
+    occ config:app:set onlyoffice verify_peer_off --value="true"
+  fi
   occ config:system:set onlyoffice jwt_secret --value="${NX_ONLYOFFICE_JWT:?"NX_ONLYOFFICE_JWT is unset"}"
   occ config:system:set onlyoffice jwt_header --value="${NX_ONLYOFFICE_JWT_HEADER:-"Authorization"}"
 }
@@ -13,6 +20,9 @@ occ_onlyoffice_remove() {
   remove_app onlyoffice
 
   occ config:app:delete onlyoffice DocumentServerUrl
+  occ config:app:delete onlyoffice DocumentServerInternalUrl
+  occ config:app:delete onlyoffice StorageUrl
+  occ config:app:delete onlyoffice verify_peer_off
   occ config:system:delete onlyoffice jwt_secret
   occ config:system:delete onlyoffice jwt_header
 }
