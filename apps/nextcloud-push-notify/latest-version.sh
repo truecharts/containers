25ee786b93becc1bb2839891be7565e78ae5e78c
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-01-21 15:50:29 +0200

    Update latest-version.sh

--- apps/nextcloud-push-notify/latest-version.sh
+++ apps/nextcloud-push-notify/latest-version.sh
@@ -10,8 +10,6 @@ notify_push_version="$(
 		| tail -1
 )"
 
-echo "updating notify_push $notify_push_version"
-
 curr_dir=./apps/nextcloud-push-notify
 sed -re 's/^ENV NOTIFY_PUSH_VERSION .*$/ENV NOTIFY_PUSH_VERSION '"$notify_push_version"'/;' -i "$curr_dir/Dockerfile"
-echo "$notify_push_version" > "$curr_dir/VERSION"
+echo "$notify_push_version"
