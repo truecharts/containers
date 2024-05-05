7a54baeb9edc361fbf0e7c2d2de0e00d6d548f5e
Stavros kois <s.kois@outlook.com>
2024-05-05 19:27:22 +0300

    remove uneeded files

--- apps/traefik/latest-version.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-version=$(head -1 ./apps/traefik/Dockerfile | grep "FROM traefik:" | cut -d ':' -f2 | cut -d '@' -f1 | cut -d '-' -f1 | cut -d 'v' -f2)
-printf "%s" "${version}"
